8cee62a4af5af5a9274f7d72a44f9761635cdc75
Avuyile Mgxotshwa <mavu072@gmail.com>
2024-06-07 00:36:55 +0200

    fix handle not found

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ def main():
 
     # Wait for user input
     command = input()
-    sys.stdout.write(f"{command}: command not found")
+    sys.stdout.write(f"{command}: command not found\n")
 
 if __name__ == "__main__":
     main()
