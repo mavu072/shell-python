0688a6701d6403c0ec261a68a85009294a2a836c
Avuyile Mgxotshwa <mavu072@gmail.com>
2024-06-07 02:07:20 +0200

    run tests

--- app/main.py
+++ app/main.py
@@ -9,5 +9,11 @@ def main():
     command = input()
     sys.stdout.write(f"{command}: command not found\n")
 
+    # Exit or REPL
+    if command == "exit":
+        sys.stdout.write(f"exited\n")
+    else:
+        main()
+
 if __name__ == "__main__":
     main()
