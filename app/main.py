56d85cb4e519d576c8ac8c4b63321d5ae9f80010
Avuyile Mgxotshwa <mavu072@gmail.com>
2024-06-07 03:28:56 +0200

    run tests

--- app/main.py
+++ app/main.py
@@ -6,26 +6,41 @@ def main():
     sys.stdout.flush()
 
     # Builtins
-    builtins = ["exit 0", "echo"]
+    builtins = ["exit", "echo", "type"]
 
     # Wait for user input
     command = input()
 
+    # Exit command
+    if command == "exit 0":
+        sys.exit(0)
+
     # Echo command
-    if command.startswith("echo"):
+    elif command.startswith("echo"):
         args = command.removeprefix("echo").strip()
         command = "echo"
         sys.stdout.write(f"{args}\n")
+
+    # Type command
+    elif command.startswith("type"):
+        args = command.removeprefix("type").strip()
+        command = "type"
+
+        # Args
+        if args in builtins:
+            sys.stdout.write(f"{args} is a shell builtin\n")
+        elif args == "cat":
+            sys.stdout.write(f"{args} is /bin/cat\n")
+        else:
+            # Will be handled in not found
+            command = args
 
     # Command not found
     if command not in builtins:
         sys.stdout.write(f"{command}: command not found\n")
 
-    # REPL or Exit
-    if command == "exit 0":
-        sys.exit(0)
-    else:
-        main()
+    # REPL
+    main()
 
 if __name__ == "__main__":
     main()
