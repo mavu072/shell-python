0ab88e594bee49954d964822b70f7b4932528264
Avuyile Mgxotshwa <mavu072@gmail.com>
2024-06-08 18:53:20 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ def main():
     sys.stdout.flush()
 
     # Builtins
-    builtins = ["exit", "echo", "type"]
+    builtins = ["exit", "echo", "type", "pwd", "cd"]
 
     # PATH
     PATH = os.environ.get("PATH")
@@ -47,31 +47,44 @@ def main():
         # Execute file with command and arguments
         os.system(commandline)
 
-    # Echo command
-    elif command == "echo":
-        sys.stdout.write(f"{args}\n")
+    # Builtins
+    elif command in builtins:
+        # Handle builtin command
+        match command:
+            # CD command
+            case "cd":
+                if os.path.exists(args):
+                    os.chdir(args)
+                else:
+                    sys.stdout.write(f"{args}: No such file or directory\n")
+            # PWD command
+            case "pwd":
+                cwd = os.getcwd()
+                sys.stdout.write(f"{cwd}\n")
+            # Echo command
+            case "echo":
+                sys.stdout.write(f"{args}\n")
+            # Type command
+            case "type":
+                # Find executable in PATH 
+                executable = None
+                for pathvar in pathvars:
+                    if os.path.isfile(f"{pathvar}/{args}"):
+                        executable = f"{pathvar}/{args}"
+                        break
 
-    # Type command
-    elif command == "type":
-        # Find executable in PATH 
-        executable = None
-        for pathvar in pathvars:
-            if os.path.isfile(f"{pathvar}/{args}"):
-                executable = f"{pathvar}/{args}"
-                break
-
-        # Builtins
-        if args in builtins:
-            sys.stdout.write(f"{args} is a shell builtin\n")
-        # Executables - In PATH
-        elif executable is not None:
-            sys.stdout.write(f"{args} is {executable}\n")
-        # Not found
-        else:
-            sys.stdout.write(f"{args} not found\n")
+                # Builtins
+                if args in builtins:
+                    sys.stdout.write(f"{args} is a shell builtin\n")
+                # Executables - In PATH
+                elif executable is not None:
+                    sys.stdout.write(f"{args} is {executable}\n")
+                # Not found
+                else:
+                    sys.stdout.write(f"{args} not found\n")
 
     # Command not found
-    elif command not in builtins:
+    else:
         sys.stdout.write(f"{command}: command not found\n")
 
     # REPL
