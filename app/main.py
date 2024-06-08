d7337ac957828b436ae7506311e61ee3b9ce4432
Avuyile Mgxotshwa <mavu072@gmail.com>
2024-06-08 15:27:54 +0200

    run tests

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import sys
-
+import os
 
 def main():
     sys.stdout.write("$ ")
@@ -7,6 +7,12 @@ def main():
 
     # Builtins
     builtins = ["exit", "echo", "type"]
+
+    # PATH variable
+    PATH = os.environ["PATH"]
+
+    # Executables
+    executables = PATH.split(":")
 
     # Wait for user input
     command = input()
@@ -26,13 +32,25 @@ def main():
         args = command.removeprefix("type").strip()
         command = "type"
 
-        # Args
-        if args in builtins:
+        # Find executable in PATH 
+        executable = None
+        for pathvar in executables:
+            if pathvar.endswith(args):
+                executable = pathvar
+                break
+
+        # Executables - In PATH
+        if executable is not None:
+            sys.stdout.write(f"{args} is {executable}\n")
+        # Builtins
+        elif args in builtins:
             sys.stdout.write(f"{args} is a shell builtin\n")
+        # Cat
         elif args == "cat":
             sys.stdout.write(f"{args} is /bin/cat\n")
+        # Not found
         else:
-            sys.stdout.write(f"{args}: not found\n")
+            sys.stdout.write(f"{args}: command not found\n")
 
     # Command not found
     elif command not in builtins:
